46aedc315630741714b21bd87c6c29b534ef0a64
kravic <vladimir@kravic.com>
2016-01-14 11:49:13 +0100

    Add License and Readme files

--- Plugins/GP SumoMe/GPSumoMe.php
+++ Plugins/GP SumoMe/GPSumoMe.php
@@ -2,7 +2,7 @@
 /**
  * @version    $version 1.0 Gospel Powered
  * @copyright  Copyright (C) 2016 Gospel Powered (http://www.gospelpowered.com) All rights reserved.
- * @license    GNU/GPL, see LICENSE.php
+ * @license    GNU/GPL, see LICENSE.txt
  *
  * Email: support@gospelpowered.com
  *
@@ -10,7 +10,6 @@
  * to the GNU General Public License, and as distributed it includes or
  * is derivative of works licensed under the GNU General Public License or
  * other free or open source software licenses.
- * See COPYRIGHT.php for copyright notices and details.
  *
  */
 
@@ -19,12 +18,12 @@ defined('_JEXEC') or die('Restricted access');
 
 jimport('joomla.plugin.plugin');
 
-class plgSystemGPSumoMeLogin extends JPlugin
+class plgSystemGPSumoMe extends JPlugin
 {
-    function plgGPSumoMeLogin(&$subject, $config)
+    function plgGPSumoMe(&$subject, $config)
     {
         parent::__construct($subject, $config);
-        $this->_plugin = JPluginHelper::getPlugin('system', 'GPSumoMeLogin');
+        $this->_plugin = JPluginHelper::getPlugin('system', 'GPSumoMe');
         $this->_params = new JParameter($this->_plugin->params);
     }
 
---
+++ Plugins/GP SumoMe/README.md
@@ -0,0 +1,5 @@
+# Joomla GP SumoMe Plugin
+
+Simple Joomla plugin that allows site owner to register and login with free SumoMe account.
+
+Supported versions: Joomla! 3.x
